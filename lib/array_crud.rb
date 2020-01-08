e6ecd6b4a6226d39bc746c602820260d7a1f47a8
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-08 00:52:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow", "Jack", "Jill", "arrays!"]
+  array = ["wow", "Jack", "am", "arrays!"]
   return array[index_number]
 end
 
