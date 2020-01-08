577cd2f1bfcce96f119ceb3f1d54cd77f1ce678e
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-08 00:35:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["Joanna", "Jack", "Jill", "John"]
-  array.unshift("#{element}")
+  array.shift("#{element}")
 end
 
 def retrieve_element_from_index(array, index_number)
