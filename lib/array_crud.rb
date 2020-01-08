68a22971bb5e17a462c8ae41f672e5e4cd3dcd31
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-08 00:41:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,10 +33,10 @@ end
 
 def retrieve_first_element_from_array(array)
   array = ["Joanna", "Jack", "Jill", "John"]
-  array.unshift("#{element}")
+  puts "#{array[0]}"
 end
 
 def retrieve_last_element_from_array(array)
   array = ["Joanna", "Jack", "Jill", "John"]
-  array.unshift("#{element}")
+  puts "#{array[-1]}"
 end
